316cba6b96cd53f0dafed97bf287192c2b68815a
hiroqn <hiroqn1008@gmail.com>
2016-06-02 14:21:11 +0900

    feat(gulp file): add gulp 😇

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const xo = require('gulp-xo');
+const plumber = require('gulp-plumber');
+const del = require('del');
+
+gulp.task('default', gulp.series(
+  () => del(['lib'], {dot: true}),
+  () => gulp.src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('lib'))));
+
+const lint = () =>
+  gulp.src(['src/**/*.js', 'gulpfile.js'])
+    .pipe(plumber())
+    .pipe(xo());
+
+gulp.task('test', () => gulp.src(['src/**/*.js', 'gulpfile.js']).pipe(xo()));
+
+gulp.task('watch', gulp.series(lint, () => {
+  gulp.watch(['src/**/*.js', 'gulpfile.js'], lint);
+}));
