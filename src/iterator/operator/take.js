af55892eb1d9ae152615c93a8714f982df648147
hiroqn <hiroqn1008@gmail.com>
2016-06-24 18:40:08 +0900

    feat(iterator/take): add iterator take

---
+++ src/iterator/operator/take.js
@@ -0,0 +1,13 @@
+export function * take(n) {
+  if (n <= 0) {
+    return;
+  }
+  let i = 0;
+  for (const value of this()) {
+    if (i >= n) {
+      return;
+    }
+    yield value;
+    i += 1;
+  }
+}
