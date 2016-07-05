fc95034acaae46edf0ee8fa9152b1c930bb97f31
hiroqn <hiroqn1008@gmail.com>
2016-07-05 16:47:46 +0900

    chore(take-until): rename file

--- src/generator/operator/take-until.js
+++ src/generator/operator/take-until.js
@@ -1,4 +1,4 @@
-import {takeUntil as _takeUntil} from '../../iterator/operator/takeUntil';
+import {takeUntil as _takeUntil} from '../../iterator/operator/take-until';
 
 export function takeUntil(predicate) {
   return () => _takeUntil.call(this(), predicate);
